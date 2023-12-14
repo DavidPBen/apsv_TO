65c002fef14e02a194c3f2eb613514e356d309b2
DavidPBen <david.plaza.benito@alumnos.upm.es>
2023-12-14 15:59:24 +0100

    First commit CI

---
+++ src/main/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplication.java
@@ -0,0 +1,15 @@
+package es.upm.dit.apsv.transportationorderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
+@SpringBootApplication
+public class TransportationOrderServerApplication {
+        //public static final Logger log = LoggerFactory.getLogger(TransportationOrderServerApplication.class);
+        private TransportationOrderRepository torderRepository;
+
+        public static void main(String[] args) {
+                SpringApplication.run(TransportationOrderServerApplication.class, args);
+        }
+}
